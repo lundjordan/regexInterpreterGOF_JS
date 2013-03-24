a7b83aa5cf3a6dea8d5f519daf554fa132adb8f2
Jordan Lund <jordan.l.lund@gmail.com>
2013-03-24 00:13:24 +0000

    initial commit. untested regex interpreter seemingly works

---
+++ regexInterpreter.js
@@ -0,0 +1,140 @@
+var regex = regex || {}
+
+// Stream obj
+regex.Stream = function(stringValue, pointerIndex) {
+    this.stringValue = stringValue;
+    this.pointerIndex = pointerIndex || 0;
+};
+regex.Stream.prototype.nextAvailable = function(pointerAdvanceLength) {
+    var returnString, startSubstrIndex = this.pointerIndex,
+        finishSubstrIndex = this.pointerIndex + pointerAdvanceLength;
+    returnString = this.stringValue.substr(startSubstrIndex, pointerAdvanceLength);
+    this.pointerIndex = finishSubstrIndex;
+    return returnString;
+};
+
+// abstract parent
+regex.RegularExpression = function() {
+};
+regex.RegularExpression.prototype.match = function(inputState) {
+};
+regex.RegularExpression.prototype.and = function(aNode) {
+    return new regex.SequenceExpression(this, aNode.asRExp());
+};
+regex.RegularExpression.prototype.repeat = function() {
+    return new regex.RepetitionExpression(this);
+};
+regex.RegularExpression.prototype.or = function(aNode) {
+    return new regex.AlternationExpression(this, aNode.asRExp());
+};
+regex.RegularExpression.prototype.asRExp = function() {
+    return this;
+};
+//
+
+
+// XXX HARDCORE JS MONKEY-PATCHING
+String.prototype.and = function(aNode) {
+    return new regex.SequenceExpression(this.asRExp(), aNode.asRExp());
+};
+String.prototype.repeat = function() {
+    return new regex.RepetitionExpression(this.asRExp());
+};
+String.prototype.or = function(aNode) {
+    return new regex.AlternationExpression(this.asRExp(), aNode.asRExp());
+};
+String.prototype.asRExp = function(inputState) {
+    return new regex.LiteralExpression(this.valueOf());
+};
+//
+
+// literalExpression
+regex.LiteralExpression = function(literal) {
+    this._components = literal; // string for comparing
+};
+regex.LiteralExpression.prototype = new regex.RegularExpression();
+regex.LiteralExpression.prototype.match = function(inputState) {
+    var finalState = [], tStream, nextAvailable;
+    for (var i = 0, n = inputState.length; i < n; i++) {
+        tStream = new regex.Stream(inputState[i].stringValue, inputState[i].pointerIndex);
+        nextAvailable = tStream.nextAvailable(this._components.length);
+        if (nextAvailable === this._components) {
+            finalState.push(tStream);
+        }
+    }
+    return finalState;
+};
+//
+
+// sequenceExpression
+regex.SequenceExpression = function(expression1, expression2) {
+    this._expression1 = expression1;
+    this._expression2 = expression2;
+};
+regex.SequenceExpression.prototype = new regex.RegularExpression();
+regex.SequenceExpression.prototype.match = function(inputState) {
+    return this._expression2.match(this._expression1.match(inputState));
+};
+
+// alternationExpression
+regex.AlternationExpression = function(alternative1, alternative2) {
+    this._alternative1 = alternative1;
+    this._alternative2 = alternative2;
+};
+regex.AlternationExpression.prototype = new regex.RegularExpression();
+regex.AlternationExpression.prototype.match = function(inputState) {
+    var finalState;
+    finalState = this._alternative1.match(inputState);
+    finalState = finalState.concat(this._alternative2.match(inputState));
+    return finalState;
+};
+
+
+// repetitionExpression
+regex.RepetitionExpression = function(repetition) {
+    this._repetition = repetition;
+};
+regex.RepetitionExpression.prototype = new regex.RegularExpression();
+regex.RepetitionExpression.prototype.match = function(inputState) {
+    var finalState = [], aState = inputState, tStream;
+
+    // console.log(aState);
+    // now let's assign finalState as a deep copy of aState
+    // in a really manual hard coded sad way for lack of knowing any better :(
+    for (var i = 0, n = aState.length; i < n; i++) {
+        tStream = new regex.Stream(aState[i].stringValue, aState[i].pointerIndex);
+        finalState.push(tStream);
+    }
+
+    while (aState.length > 0) {
+        aState = this._repetition.match(aState);
+        finalState = finalState.concat(aState);
+    };
+
+    return finalState;
+};
+
+
+
+// regEx1 = "a".repeat().and("abc").or("dog");
+
+regex.compile = function(pattern, stringInput) {
+    inputState = [new regex.Stream(stringInput)];
+    matches = pattern.match(inputState)
+    for (var i = 0, n = matches.length; i < n; i++) {
+        streamingCharsUnmatched = matches[i].stringValue.length -
+            matches[i].pointerIndex;
+        if (streamingCharsUnmatched === 0) {
+            return "result: found match\nPattern: " + pattern + 
+                "\nInput: " + stringInput + "\n";
+        }
+    }
+    return "result: no match found\nPattern: " + pattern + 
+        "\nInput: " + stringInput + "\n";
+};
+
+result = regex.compile("a".repeat().and("abc"), "aaa");
+result2 = regex.compile("a".repeat().and("abc"), "aaaabc");
+
+console.log(result);
+console.log(result2);
